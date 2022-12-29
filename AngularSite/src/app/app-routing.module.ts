b8b1bb542cb5fc4dba08b5fa730fb18fa3e582f4
Liamarii <39436443+Liamarii@users.noreply.github.com>
2022-12-29 16:37:31 +0000

    made a basic navigation bar using TailwindCss

--- AngularSite/src/app/app-routing.module.ts
+++ AngularSite/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'weather', component: WeatherComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'about', component: AboutComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
