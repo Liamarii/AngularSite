3c80f7f62900b0c7699e5f131a07f91d9beac4cf
Liam Costello <liamdcostello@gmail.com>
2024-05-03 18:57:34 +0100

    subscription missing an unsubscribe

--- AngularSite/src/app/components/navigation-bar/navigation-bar.component.ts
+++ AngularSite/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
 })
-export default class NavigationBarComponent {
+export default class NavigationBarComponent implements OnDestroy {
   displayMenu = true;
   smallScreen = false;
   pages = ['form', 'typography', 'cats', 'jokes', 'products', 'error'];
-  title = "AngularSite"
+  title = "AngularSite";
+  private breakpointSubscription: Subscription;
 
   ToggleMenuVisibility() {
     this.displayMenu = !this.displayMenu;
   }
 
   constructor(private breakpointObserver: BreakpointObserver) {
-    this.breakpointObserver.observe(["(min-width: 640px)"]).subscribe((result: BreakpointState) => {
+    this.breakpointSubscription = this.breakpointObserver.observe(["(min-width: 640px)"]).subscribe((result: BreakpointState) => {
       if (result.matches) {
         this.displayMenu = true;
         this.smallScreen = false;
@@ -25,5 +27,11 @@ export default class NavigationBarComponent {
         this.smallScreen = true;
       }
     });
+  }
+
+  ngOnDestroy() {
+    if (this.breakpointSubscription) {
+      this.breakpointSubscription.unsubscribe();
+    }
   }
 }
